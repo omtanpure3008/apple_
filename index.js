07700b0dc7003b1941276b8a65d8bdecce51f306
omtanpure3008 <162991862+omtanpure3008@users.noreply.github.com>
2024-07-09 20:17:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,101 @@
+let uddhav = new Swiper(".mySwiper", {
+    slidesPerView: 8,
+    slidesPerGroup: 1,
+    spaceBetween: 20,
+    loop: true,
+    keyboard: {
+        enabled: true,
+    },
+    // mousewheel: true,
+    breakpoints: {
+        320: {
+            slidesPerView: 3,
+        },
+        375: {
+            slidesPerView: 3,
+        },
+        425: {
+            slidesPerView: 3,
+        },
+        768: {
+            slidesPerView: 4,
+        },
+        1024: {
+            slidesPerView: 8,
+        }
+    }
+})
+
+
+let kiran = new Swiper(".mySwiper2", {
+    slidesPerView: 5,
+    spaceBetween: 20,
+    // loop: true,
+    // mousewheel: true,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        375: {
+            slidesPerView: 1,
+        },
+        425: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        }
+    }
+})
+
+let swiper3 = new Swiper(".mySwiper3", {
+    slidesPerView: 5,
+    spaceBetween: 20,
+    // loop: true,
+    // mousewheel: true,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        375: {
+            slidesPerView: 1,
+        },
+        425: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 2.5,
+        }
+    }
+})
+
+let swiper4 = new Swiper(".mySwiper4", {
+    slidesPerView: 5,
+    spaceBetween: 20,
+    // loop: true,
+    // mousewheel: true,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        375: {
+            slidesPerView: 1,
+        },
+        425: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3.8,
+        }
+    }
+})
+
